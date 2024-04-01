051df51571ff4e8fb19f3d2f8bd1661cb6bd3231
Panjiiiiiii <110644169+Panjiiiiiii@users.noreply.github.com>
2024-04-01 09:41:25 +0700

    test

--- Frontend/src/Components/Home/LandingPage/LandingPage.jsx
+++ Frontend/src/Components/Home/LandingPage/LandingPage.jsx
@@ -1,79 +1,84 @@
-import React, { useState } from 'react';
-import './LandingPage.css';
-import header from '../../../Assets/header.jpg'
-import Carousel from '../../Carousel/Carousel'
-import img from '../../../Assets/content2.jpg'
+import React, { useState } from "react";
+import "./LandingPage.css";
+import header from "../../../Assets/header.jpg";
+import Carousel from "../../Carousel/Carousel";
+import img from "../../../Assets/content2.jpg";
 
 const LandingPage = () => {
-    return (
-        <div className="home">
-            <div className="img">
-                <img src={header} alt=""/>            
-            </div>
-            <section className="grid ">
-                <div className="horizontal-grid d-flex flex-row">
-                    <div className="checkIn ">
-                        <label htmlFor="checkIn">Check In</label>
-                        <input type="date" id="checkIn" />
-                    </div>
+  return (
+    <div className="home">
+      <div className="img">
+        <img src={header} alt="" />
+      </div>
+      <section className="grid ">
+        <div className="horizontal-grid d-flex flex-row">
+          <div className="checkIn ">
+            <label htmlFor="checkIn">Check In</label>
+            <input type="date" id="checkIn" />
+          </div>
 
-                    <div className="checkOut">
-                        <label htmlFor="checkOut">Check Out</label>
-                        <input type="date" id="checkOut" />
-                    </div>
+          <div className="checkOut">
+            <label htmlFor="checkOut">Check Out</label>
+            <input type="date" id="checkOut" />
+          </div>
 
-                    <div className="roomGuest">
-                        <label htmlFor="roomGuest">Room & Guest</label>
-                        <select className='dropdown' id='roomGuest'>
-                            <option value="javascript"></option>
-                            <option value="room">Room</option>
-                            <option value="tipekamar">Tipe Kamar</option>
-                            <option value="nomorkamar">Nomor Kamar</option>
-                            <option value="jumlahkamar">Jumlah Kamar</option>
+          <div className="roomGuest">
+            <label htmlFor="roomGuest">Room & Guest</label>
+            <select className="dropdown" id="roomGuest">
+              <option value="javascript"></option>
+              <option value="room">Room</option>
+              <option value="tipekamar">Tipe Kamar</option>
+              <option value="nomorkamar">Nomor Kamar</option>
+              <option value="jumlahkamar">Jumlah Kamar</option>
+            </select>
+          </div>
 
-                        </select>
-                    </div>
+          <button className="btn">
+            <a href="#">Find a Four Points</a>
+          </button>
+        </div>
+      </section>
+      <div className="textContent">
+        <h1 className="title1">Four Points</h1>
+        <h6 className="subttitle2">by Sheraton Munich Arabellapark</h6>
+        <p className="desk">
+          Discover our new hotel that combines Bavarian charm and a timeless
+          design with local elements. Located a few subway stops from the city
+          center and only a short walk from the English Garden, Munich’s famous
+          city park, you will experience the special mixture of exciting city
+          life and Bavarian coziness that makes Munich so unique. Inside, find
+          convenient workspaces with fast, free Wi-Fi, Munich’s highest spa with
+          pool, sauna, and treatments, and local beer with Best Brews™ on tap.
+          Spacious guest rooms with views up to the alps round out your stay.
+        </p>
+      </div>
 
-                    <button className='btn'>
-                        <a href="#">Find a Four Points</a>
-                    </button>
-                </div>
+      <Carousel />
 
-            </section> 
-           <div className="textContent">
-                <h1 className="title1">Four Points</h1>
-                <h6 className='subttitle2'>by Sheraton Munich Arabellapark</h6>
-                <p className='desk'>
-                    Discover our new hotel that combines Bavarian charm and a timeless design with local elements.
-                    Located a few subway stops from the city center and only a short walk from the English Garden, Munich’s
-                    famous city park, you will experience the special mixture of exciting city life and Bavarian coziness that makes Munich so unique.
-                    Inside, find convenient workspaces with fast, free Wi-Fi, Munich’s highest spa with pool, sauna, and treatments, and local beer
-                    with Best Brews™ on tap. Spacious guest rooms with views up to the alps round out your stay.
-                </p>
-            </div>
+      <div className="content3">
+        <div className="img">
+          <img src={img} alt="" />
+        </div>
 
-            <Carousel/>
+        <div className="text">
+          <h1>
+            In the realm of 30 extraordinary Marriott Bonvoy hotel brands, Four
+            Points is a standout.
+          </h1>
+          <p>
+            Step into a world of exclusive member advantages with Marriott
+            Bonvoy™, featuring complimentary Wi-Fi, member-only rates, and
+            points that unlock travel experiences. Start your adventure at
+            Marriott.com. Where will our journey together lead?
+          </p>
+          <div className="link">
+            <a href="../../Login/Login">Sign in </a>
+            <a href="">Join Now </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-            <div className="content3">
-                <div className="img">
-                    <img src={img} alt="" />
-                </div>
-
-                <div className="text">
-                <h1>In the realm of 30 extraordinary Marriott Bonvoy hotel brands, Four Points is a standout.</h1>
-                <p>Step into a world of exclusive member advantages with Marriott Bonvoy™, featuring complimentary Wi-Fi, member-only rates, and points that unlock travel experiences. Start your adventure at Marriott.com. Where will our journey together lead?</p>
-                <div className='link'>
-                <a href="../../Login/Login">Sign in  </a>
-                <a href="">Join Now  </a>
-                </div>
-                </div>
-
-
-            </div>
-
-            </div>
-       
-       );  
-   };
-   
-   export default LandingPage;
+export default LandingPage;
--- Frontend/src/Components/Login/Login.jsx
+++ Frontend/src/Components/Login/Login.jsx
@@ -1,93 +1,105 @@
-import React, { useState } from 'react';
-import './Login.css';
-import logo1 from '../../Assets/logo.jpg';
+import React, { useState } from "react";
+import "./Login.css";
+import logo1 from "../../Assets/logo.jpg";
+import axios from "axios";
 
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { email: '' };
-        this.state = { pass: '' };
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-        this.handleChangeEmail = this.handleChangeEmail.bind(this);
-        this.handleChangePass = this.handleChangePass.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  const handleChangePass = async (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:7000/auth", {
+        email,
+        password,
+      });
+
+      if (response.data.success) {
+        console.log("Login successful : ", response.data);
+      } else {
+        console.log("Login error : ", response.data.message);
+      }
+    } catch (error) {
+      console.log("Login error : ", error);
     }
+  };
+  return (
+    <div>
+      <div class="background"></div>
 
-    handleChangeEmail(event) {
-        this.setState({ email: event.target.email });
-    }
-
-    handleChangePass(event) {
-        this.setState({ pass: event.target.pass });
-    }
-
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <div>
-                <div class="background">
-
-                </div>
-
-                <div class="content">
-                    <div class="container-1">
-                        <br />
-                        <br />
-                        <div class="box">
-                            <div class="header">
-                                <div class="text">
-                                    <h1> Local concepts with a</h1>
-                                    <h1> Global reach</h1>
-                                </div>
-                                <div class="logo1">
-                                    <img src={logo1} className='logo2' alt="logo1"></img>
-                                </div>
-                            </div>
-                            <div class="form">
-                                <div class="item">
-                                    <form onSubmit={this.handleSubmit}>
-                                        <label class="tittle">
-                                            Email
-                                        <br />
-                                            <input class="form-login" type="email" placeholder="Insert Email" value={this.state.value} onChange={this.handleChangeEmail} />
-                                        </label>
-                                        <br />
-
-                                        <label class="tittle">
-                                            Password
-                                        <br />
-                                            <input class="form-login" type="Password" placeholder="Insert Password" value={this.state.value} onChange={this.handleChangePass} />
-                                        </label>
-                                        <div class="col-sm-10 offset-2">
-                                            <input type="checkbox" class="form-check-input" /> Tampilkan Password
-                                            </div>
-                                        <br />
-                                        <input class="submit-btn" type="submit" value="Login" />
-                                    </form>
-                                    <br />
-                                    <div>
-                                        Belum memiliki akun? <a href="#" class="text-decoration-none"> Sign In</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <br />
-                        <br />
-                    </div>
-                </div>
-
+      <div class="content">
+        <div class="container-1">
+          <br />
+          <br />
+          <div class="box">
+            <div class="header">
+              <div class="text">
+                <h1> Local concepts with a</h1>
+                <h1> Global reach</h1>
+              </div>
+              <div class="logo1">
+                <img src={logo1} className="logo2" alt="logo1"></img>
+              </div>
             </div>
+            <div class="form">
+              <div class="item">
+                <form onSubmit={this.handleSubmit}>
+                  <label class="tittle">
+                    Email
+                    <br />
+                    <input
+                      class="form-login"
+                      type="email"
+                      placeholder="Insert Email"
+                      value={this.state.value}
+                      onChange={this.handleChangeEmail}
+                    />
+                  </label>
+                  <br />
 
-
-        );
-
-    }
-}
-
-
+                  <label class="tittle">
+                    Password
+                    <br />
+                    <input
+                      class="form-login"
+                      type="Password"
+                      placeholder="Insert Password"
+                      value={this.state.value}
+                      onChange={this.handleChangePass}
+                    />
+                  </label>
+                  <div class="col-sm-10 offset-2">
+                    <input type="checkbox" class="form-check-input" /> Tampilkan
+                    Password
+                  </div>
+                  <br />
+                  <input class="submit-btn" type="submit" value="Login" />
+                </form>
+                <br />
+                <div>
+                  Belum memiliki akun?{" "}
+                  <a href="#" class="text-decoration-none">
+                    {" "}
+                    Sign In
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Login;
